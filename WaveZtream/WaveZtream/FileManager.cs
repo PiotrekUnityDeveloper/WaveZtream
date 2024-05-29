3996b80c079a3a811552e5eb84f0d4c6b792d9b2
PiotrekUnityDeveloper <85606121+PiotrekUnityDeveloper@users.noreply.github.com>
2024-05-29 19:20:29 +0200

    im too fucking tired right now to start this

---
+++ WaveZtream/WaveZtream/FileManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaveZtream
+{
+    internal class FileManager
+    {
+        public static void LoadLyricsForAudioDefinition(AudioDefinition definition)
+        {
+
+        }
+    }
+}
