a4f33cbf7a07ff1c45f7641963a0fe47bb67f9fc
PiotrekUnityDeveloper <85606121+PiotrekUnityDeveloper@users.noreply.github.com>
2024-06-02 15:00:57 +0200

    tought lyrics work

--- WaveZtream/WaveZtream/FileManager.cs
+++ WaveZtream/WaveZtream/FileManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Krypton.Toolkit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,5 +13,45 @@ namespace WaveZtream
         {
 
         }
+    }
+
+    public class Lyrics
+    {
+        public class StandardLyrics
+        {
+            public string lyricString;
+            public List<LyricEntry> lyricEntries;
+
+            //file specifics
+            public string fileExtension;
+            // Supported file types:
+            // /text formats:                     .txt .text .rtf
+            // /lyric-focused formats:            .lrc .kra .audlrc
+            // /subtitle-focused formats:         .srt .sub .smi
+
+            public string timestampFormat = "hh:mm:ss:xx";
+            //h- hours, m- minutes, s- seconds, x- miliseconds
+
+
+            private TimeSpan lyricsDuration;
+
+            public StandardLyrics() { InitializeLyrics(); }
+
+            public void InitializeLyrics()
+            {
+
+            }
+        }
+
+        public class WaveLyrics //aka. AdvancedLyrics/Visualized Lyrics
+        {
+            //TODO
+        }
+    }
+
+    public class LyricEntry
+    {
+        public TimeSpan entryTime { get; set; } 
+        public string entryText { get; set; }
     }
 }
